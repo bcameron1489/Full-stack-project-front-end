09746689b9176468613ac1d2d2f27b37aed60828
bcameron1489 <bcameron1489@gmail.com>
2017-12-03 23:39:49 -0500

    Revert changes made to ui final commit

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,26 +4,17 @@ const showPlayersHtml = require('./templates/show-players-html.hbs')
 const showUserPlayersHtml = require('./templates/show-user-players.hbs')
 const getSingleResource = require('./templates/get-single-resource.hbs')
 
-const clearAlerts = function () {
-  $('.update-alert').html('')
-  $('.user-alerts').html('')
-  $('.alerts').html('')
-}
-
 const onSignUpSuccess = function () {
-  clearAlerts()
   $('.alerts').text('Successfully Signed Up')
   $('.signup').val('')
 }
 
 const onSignUpFailure = function () {
-  clearAlerts()
   $('.alerts').text('Failed To Signed Up')
   $('.input').val('')
 }
 
 const onSignInSuccess = function (data) {
-  clearAlerts()
   $('.alerts').text('Successfully Signed In')
   $('.input').val('')
   $('.login-forms').hide()
@@ -34,13 +25,11 @@ const onSignInSuccess = function (data) {
 }
 
 const onSignInFailure = function (data) {
-  clearAlerts()
   $('.alerts').text('Failed To Sign In')
   $('.input').val('')
 }
 
 const onSignOutSuccess = function (data) {
-  clearAlerts()
   $('.alerts').text('Successfully Signed Out')
   $('.update-alert').html('')
   $('.user-alerts').html('')
@@ -56,24 +45,20 @@ const onSignOutSuccess = function (data) {
 }
 
 const onSignOutFailure = function (data) {
-  clearAlerts()
   $('.alerts').text('Failed To Sign Out')
 }
 
 const changePasswordSuccess = function () {
-  clearAlerts()
   $('.alerts').text('Successfully Changed Password')
   $('.input').val('')
 }
 
 const changePasswordFailure = function () {
-  clearAlerts()
   $('.alerts').text('Failed To Change Password')
   $('.input').val('')
 }
 
 const getPlayersSuccess = function (data) {
-  clearAlerts()
   $('.alerts').text('Successfully Retrieved Players')
   $('.clear-input').html('')
   const showPlayers = showPlayersHtml({ players: data.players })
@@ -86,7 +71,6 @@ const getPlayersSuccess = function (data) {
 }
 
 const getSinglePlayerSuccess = function (data) {
-  clearAlerts()
   $('.alerts').text('Successfully Retrieved Single Resource')
   $('.clear-input').val('')
   const singleResource = getSingleResource({ players: data })
@@ -98,17 +82,14 @@ const getSinglePlayerSuccess = function (data) {
 }
 
 const getSinglePlayerFailure = function (data) {
-  clearAlerts()
   $('.alerts').text('Successfully Retrieved Single Resource')
 }
 
 const getPlayersFailure = function () {
-  clearAlerts()
   $('.alerts').text('Failed To Get Index')
 }
 
 const userIndexSuccess = function (data) {
-  clearAlerts()
   $('.user-alerts').text('Successfully Retrieved Your Players')
   const showUserPlayers = showUserPlayersHtml({ fantasy_players: data.fantasy_players })
   $('.create-content').html('')
@@ -116,35 +97,29 @@ const userIndexSuccess = function (data) {
 }
 
 const userIndexFailure = function () {
-  clearAlerts()
   $('.alerts').text('Failed To Get Your Players')
 }
 
 const addUserPlayerSuccess = function (data) {
-  clearAlerts()
   $('.alerts').text('Successfully Added Player')
   $('.clear-input').val('')
   store.fantasy = data.fantasy
 }
 
 const addUserPlayerFailure = function (data) {
-  clearAlerts()
   $('.alerts').text('Failed To Add Player')
 }
 
 const deletePlayerSuccess = function (data) {
-  clearAlerts()
   $('.update-alert').text('Successfully Removed Player')
   $('.clear-input').val('')
 }
 
 const deletePlayerFailure = function (data) {
-  clearAlerts()
   $('.alerts').text('Failed To Remove Player')
 }
 
 const updatePlayerSuccess = function (data) {
-  clearAlerts()
   $('.clear-input').val('')
   $('.create-content').html('')
   $('.update-alert').text('Successfully Updated Target')
@@ -154,7 +129,6 @@ const updatePlayerSuccess = function (data) {
 }
 
 const updatePlayerFailure = function (data) {
-  clearAlerts()
   $('.input').val('')
   $('.alerts').text('Failed To Update')
   store.fantasy = data.fantasy
